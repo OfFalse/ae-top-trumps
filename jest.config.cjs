7d8521bd3928beeed3ef30900073e1bfbb569eac
OfFalse <n.g.j.defaux@gmail.com>
2025-10-04 10:28:59 +0100

    chore(config): linting

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,12 +1,12 @@
 /** @type {import('jest').Config} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'jsdom',
-  roots: ['<rootDir>/src'],
-  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  roots: ["<rootDir>/src"],
+  setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"],
   moduleNameMapper: {
-    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
-    '\\.(gif|ttf|eot|svg|png|jpg|jpeg)$': '<rootDir>/src/__mocks__/fileMock.js'
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+    "\\.(gif|ttf|eot|svg|png|jpg|jpeg)$": "<rootDir>/src/__mocks__/fileMock.js",
   },
-  testMatch: ['**/?(*.)+(spec|test).(ts|tsx)'],
+  testMatch: ["**/?(*.)+(spec|test).(ts|tsx)"],
 };
