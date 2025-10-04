c99a826cd2316e6b71ac72321a20587641f212c7
OfFalse <n.g.j.defaux@gmail.com>
2025-10-04 15:49:21 +0100

    feat(list): display and remove functionality

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   Table,
   TableHead,
@@ -14,27 +14,51 @@ interface ListProps {
 }
 
 const List: React.FC<ListProps> = ({ headers, items }) => {
+  const [listItems, setListItems] = useState(items || []);
+
+  const handleRemove = (e: React.MouseEvent<HTMLAnchorElement>, id: number) => {
+    e.preventDefault();
+    setListItems((prevItems) => prevItems.filter((item) => item.id !== id));
+  };
+
+  useEffect(() => {
+    setListItems(items || []);
+  }, [items]);
+
   return (
-    <Table>
-      <TableHead>
-        <TableRow>
-          {headers?.map((col) => (
-            <TableHeader key={col.key}>{col.header}</TableHeader>
-          ))}
-        </TableRow>
-      </TableHead>
-      <TableBody>
-        {items?.map((item) => (
-          <TableRow key={item.id}>
-            <td>{item.title}</td>
-            <td>{item.skillLevel}</td>
-            <td>
-              <Link>Remove</Link>
-            </td>
-          </TableRow>
-        ))}
-      </TableBody>
-    </Table>
+    <>
+      {(!headers || headers.length === 0) &&
+      (!listItems || listItems.length === 0) ? null : (
+        <Table>
+          <TableHead>
+            <TableRow>
+              {headers?.map((col) => (
+                <TableHeader key={col.key}>{col.header}</TableHeader>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {listItems?.map((item, i) => (
+              <TableRow key={item.id}>
+                <td>{item.title}</td>
+                <td>{item.skillLevel}</td>
+                <td>
+                  <Link
+                    data-testId={`remove-link-${i}`}
+                    id={`remove-link-${i}`}
+                    aria-label={`Remove ${item.title}`}
+                    href="#"
+                    onClick={(e) => handleRemove(e, item.id)}
+                  >
+                    Remove
+                  </Link>
+                </td>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      )}
+    </>
   );
 };
 
